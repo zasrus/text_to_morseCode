f2396d23df2eb2b7af19f59cbea03037b15762cf
zasrus <zasrus@gmail.com>
2024-03-29 13:33:12 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from code import code
+
+
+def main():
+  mesg = 'the time is 12:00 e-mail dick at cd@supa.com'
+  mesg_code = get_code(mesg.upper())
+  for l in mesg_code:
+    print(l, end="")
+
+
+def get_code(text):
+  message = []
+  mcode = code
+  for letter in text:
+    if letter in mcode:
+      message += f'{mcode[letter]} '
+    elif letter == ' ':
+      message.append('/')
+    elif letter not in mcode:
+      message.append(f'/{letter}/')
+  return message
+
+
+if __name__ == '__main__':
+  main()
